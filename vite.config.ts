908e84fc2c7a6f0f9b5f7a79f14e684cd4c45e30
Afaq Ahmed <afaaaq85@gmail.com>
2024-09-24 19:09:26 +0500

    change base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/configurator-deploy/',
+  base: '/vite-react-deploy/',
   server: {
     watch: {
       usePolling: true,
